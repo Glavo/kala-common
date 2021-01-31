8f1e819ef91adab98e90033a745c6e0ad722234c
Glavo <zjx001202@126.com>
2021-01-31 20:59:41 +0800

    add Seq#sliceView(int, int)

--- kala-collection/src/main/java/org/glavo/kala/collection/Seq.java
+++ kala-collection/src/main/java/org/glavo/kala/collection/Seq.java
@@ -101,6 +101,10 @@ public interface Seq<@Covariant E> extends Collection<E>, SeqLike<E> {
         return new SeqViews.Of<>(this);
     }
 
+    default @NotNull SeqView<E> sliceView(int beginIndex, int endIndex) {
+        return view().slice(beginIndex, endIndex);
+    }
+
     @Override
     default @NotNull @UnmodifiableView List<E> asJava() {
         return this instanceof IndexedSeq<?>
