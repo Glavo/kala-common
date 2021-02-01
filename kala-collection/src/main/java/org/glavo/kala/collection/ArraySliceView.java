922c987655c4d81cf54cb619cd04247f90bbdb51
Glavo <zjx001202@126.com>
2021-02-01 22:10:55 +0800

    impl ArraySliceView

---
+++ kala-collection/src/main/java/org/glavo/kala/collection/ArraySliceView.java
@@ -0,0 +1,42 @@
+package org.glavo.kala.collection;
+
+import org.glavo.kala.control.Conditions;
+import org.glavo.kala.traversable.JavaArray;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+import java.util.Iterator;
+
+@SuppressWarnings("unchecked")
+public class ArraySliceView<E> extends AbstractIndexedSeqView<E> {
+    protected final Object[] array;
+    protected final int beginIndex;
+    protected final int size;
+
+    protected ArraySliceView(Object[] array, int beginIndex, int size) {
+        this.array = array;
+        this.beginIndex = beginIndex;
+        this.size = size;
+    }
+
+    @Override
+    public String className() {
+        return "ArraySliceView";
+    }
+
+    @Override
+    public final int size() {
+        return this.size;
+    }
+
+    @Override
+    public final E get(@Range(from = 0, to = Integer.MAX_VALUE) int index) {
+        Conditions.checkElementIndex(index, size);
+        return (E) array[beginIndex + index];
+    }
+
+    @Override
+    public @NotNull Iterator<E> iterator() {
+        return (Iterator<E>) JavaArray.iterator(array, beginIndex, beginIndex + size);
+    }
+}
