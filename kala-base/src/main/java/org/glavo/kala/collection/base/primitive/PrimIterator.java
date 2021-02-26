fe155be07e9e7f184ba1ec7f6b24cbfeba31ff32
Glavo <zjx001202@126.com>
2021-02-27 04:03:24 +0800

    Add '@Contract' to PrimIterator

--- kala-base/src/main/java/org/glavo/kala/collection/base/primitive/PrimIterator.java
+++ kala-base/src/main/java/org/glavo/kala/collection/base/primitive/PrimIterator.java
@@ -32,6 +32,7 @@ public interface PrimIterator<
 
     //region Size Info
 
+    @Contract(mutates = "this")
     int size();
 
     //endregion
@@ -40,10 +41,13 @@ public interface PrimIterator<
 
     //region Element Conditions
 
+    @Contract(mutates = "this")
     boolean contains(Object value);
 
+    @Contract(mutates = "this")
     boolean containsAll(@NotNull T_ARRAY values);
 
+    @Contract(mutates = "this")
     default boolean containsAll(T @NotNull [] values) {
         for (T value : values) {
             if (!contains(value)) {
@@ -53,6 +57,7 @@ public interface PrimIterator<
         return true;
     }
 
+    @Contract(mutates = "this")
     default boolean containsAll(@NotNull Iterable<?> values) {
         for (Object value : values) {
             if (!contains(value)) {
@@ -62,54 +67,73 @@ public interface PrimIterator<
         return true;
     }
 
+    @Contract(mutates = "this")
     boolean sameElements(@NotNull T_ITERATOR other);
 
+    @Contract(mutates = "this")
     boolean sameElements(@NotNull Iterator<?> other);
 
+    @Contract(mutates = "this")
     boolean anyMatch(@NotNull T_PREDICATE predicate);
 
+    @Contract(mutates = "this")
     boolean allMatch(@NotNull T_PREDICATE predicate);
 
+    @Contract(mutates = "this")
     boolean noneMatch(@NotNull T_PREDICATE predicate);
 
     //endregion
 
     //region Misc Operations
 
+    @Contract(mutates = "this")
     @NotNull T_ITERATOR drop(int n);
 
+    @Contract(mutates = "this")
     @NotNull T_ITERATOR dropWhile(@NotNull T_PREDICATE predicate);
 
+    @Contract(mutates = "this")
     @NotNull T_ITERATOR take(int n);
 
+    @Contract(mutates = "this")
     @NotNull T_ITERATOR takeWhile(@NotNull T_PREDICATE predicate);
 
+    @Contract(mutates = "this")
     @NotNull T_ITERATOR filter(@NotNull T_PREDICATE predicate);
 
+    @Contract(mutates = "this")
     @NotNull T_ITERATOR filterNot(@NotNull T_PREDICATE predicate);
 
+    @Contract(mutates = "this")
     @NotNull Tuple2<@NotNull T_ITERATOR, @NotNull T_ITERATOR> span(@NotNull T_PREDICATE predicate);
 
     //endregion
 
     //region Aggregate Operations
 
+    @Contract(mutates = "this")
     int count(@NotNull T_PREDICATE predicate);
 
+    @Contract(mutates = "this")
     @Nullable T maxOrNull();
 
+    @Contract(mutates = "this")
     @NotNull T_OPTION maxOption();
 
+    @Contract(mutates = "this")
     @Nullable T minOrNull();
 
+    @Contract(mutates = "this")
     @NotNull T_OPTION minOption();
 
     //endregion
 
+    @Contract(mutates = "this")
     @NotNull T_ARRAY toArray();
 
     //region Traverse Operations
 
+    @Contract(mutates = "this")
     void forEach(@NotNull T_CONSUMER action);
 
     @Override
@@ -122,28 +146,33 @@ public interface PrimIterator<
 
     //region String Representation
 
+    @Contract(mutates = "this, param1")
     default <A extends Appendable> @NotNull A joinTo(@NotNull A buffer) {
         return joinTo(buffer, ", ");
     }
 
+    @Contract(mutates = "this, param1")
     default <A extends Appendable> @NotNull A joinTo(@NotNull A buffer, CharSequence separator) {
         return joinTo(buffer, separator, "", "");
     }
 
-    @Contract(value = "_, _, _, _ -> param1", mutates = "param1")
+    @Contract(value = "_, _, _, _ -> param1", mutates = "this, param1")
     <A extends Appendable> @NotNull A joinTo(
             @NotNull A buffer,
             CharSequence separator, CharSequence prefix, CharSequence postfix
     );
 
+    @Contract(mutates = "this")
     default @NotNull String joinToString() {
         return joinTo(new StringBuilder()).toString();
     }
 
+    @Contract(mutates = "this")
     default @NotNull String joinToString(CharSequence separator) {
         return joinTo(new StringBuilder(), separator).toString();
     }
 
+    @Contract(mutates = "this")
     default @NotNull String joinToString(CharSequence separator, CharSequence prefix, CharSequence postfix) {
         return joinTo(new StringBuilder(), separator, prefix, postfix).toString();
     }
