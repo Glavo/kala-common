be2c0f17064b1005185ace4a0f8c1a0668b17481
Glavo <zjx001202@126.com>
2021-02-27 13:51:43 +0800

    Cleanup code

--- kala-collection/src/main/java/org/glavo/kala/collection/AbstractCollection.java
+++ kala-collection/src/main/java/org/glavo/kala/collection/AbstractCollection.java
@@ -10,41 +10,6 @@ import java.util.function.BiConsumer;
 import java.util.stream.Collector;
 
 public abstract class AbstractCollection<@Covariant E> implements Collection<E> {
-    static <E, R, Builder> R collectTo(
-            @NotNull Collection<? extends E> collection,
-            @NotNull Collector<? super E, Builder, ? extends R> collector
-    ) {
-        Objects.requireNonNull(collector);
-
-        final BiConsumer<Builder, ? super E> accumulator = collector.accumulator();
-
-        Builder builder = collector.supplier().get();
-
-        for (E e : collection) {
-            accumulator.accept(builder, e);
-        }
-
-        return collector.finisher().apply(builder);
-    }
-
-    static <E, R, Builder> R collectTo(
-            @NotNull Collection<? extends E> collection,
-            @NotNull CollectionFactory<? super E, Builder, ? extends R> factory
-    ) {
-        Objects.requireNonNull(factory);
-
-        if (collection.isEmpty()) {
-            return factory.empty();
-        }
-
-        Builder builder = factory.newBuilder();
-
-        for (E e : collection) {
-            factory.addToBuilder(builder, e);
-        }
-
-        return factory.build(builder);
-    }
 
     @Override
     public int hashCode() {
