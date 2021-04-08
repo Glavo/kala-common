3b523bd6fe690465eb556edc1f5264773d753f42
Glavo <zjx001202@126.com>
2021-04-08 21:23:31 +0800

    Add comparators of Tuple

--- kala-base/src/main/java/org/glavo/kala/tuple/Tuple.java
+++ kala-base/src/main/java/org/glavo/kala/tuple/Tuple.java
@@ -1,6 +1,8 @@
 package org.glavo.kala.tuple;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.function.IntFunction;
 
 import org.intellij.lang.annotations.Flow;
@@ -1011,5 +1013,324 @@ public abstract class Tuple implements AnyTuple, Serializable {
                                                                                                                                                                                                                                                                                                             ?>>>>>>>>>>>>>>>>>>
                     tuple) {
         return tuple.elementAt(17);
+    }
+
+    public static @NotNull Comparator<Unit> comparator() {
+        return (Comparator<Unit> & Serializable) (u1, u2) -> 0;
+    }
+
+    public static <T1> @NotNull Comparator<Tuple1<T1>> comparator(@NotNull Comparator<? super T1> c1) {
+        return (Comparator<Tuple1<T1>> & Serializable) (t1, t2) -> c1.compare(t1.component1(), t2.component1());
+    }
+
+    public static <T1, T2> @NotNull Comparator<Tuple2<T1, T2>> comparator(
+            @NotNull Comparator<? super T1> c1,
+            @NotNull Comparator<? super T2> c2
+    ) {
+        return (Comparator<Tuple2<T1, T2>> & Serializable) (t1, t2) -> {
+            final int compare1 = c1.compare(t1._1, t2._1);
+            if (compare1 != 0) {
+                return compare1;
+            }
+            return c2.compare(t1._2, t2._2);
+        };
+    }
+
+    public static <T1, T2, T3> @NotNull Comparator<Tuple3<T1, T2, T3>> comparator(
+            @NotNull Comparator<? super T1> c1,
+            @NotNull Comparator<? super T2> c2,
+            @NotNull Comparator<? super T3> c3
+    ) {
+        return (Comparator<Tuple3<T1, T2, T3>> & Serializable) (t1, t2) -> {
+            final int compare1 = c1.compare(t1._1, t2._1);
+            if (compare1 != 0) {
+                return compare1;
+            }
+            final int compare2 = c2.compare(t1._2, t2._2);
+            if (compare2 != 0) {
+                return compare2;
+            }
+            return c3.compare(t1._3, t2._3);
+        };
+    }
+
+    public static <T1, T2, T3, T4> @NotNull Comparator<Tuple4<T1, T2, T3, T4>> comparator(
+            @NotNull Comparator<? super T1> c1,
+            @NotNull Comparator<? super T2> c2,
+            @NotNull Comparator<? super T3> c3,
+            @NotNull Comparator<? super T4> c4
+    ) {
+        return (Comparator<Tuple4<T1, T2, T3, T4>> & Serializable) (t1, t2) -> {
+            final int compare1 = c1.compare(t1._1, t2._1);
+            if (compare1 != 0) {
+                return compare1;
+            }
+            final int compare2 = c2.compare(t1._2, t2._2);
+            if (compare2 != 0) {
+                return compare2;
+            }
+            final int compare3 = c3.compare(t1._3, t2._3);
+            if (compare3 != 0) {
+                return compare3;
+            }
+            return c4.compare(t1._4, t2._4);
+        };
+    }
+
+    public static <T1, T2, T3, T4, T5> @NotNull Comparator<Tuple5<T1, T2, T3, T4, T5>> comparator(
+            @NotNull Comparator<? super T1> c1,
+            @NotNull Comparator<? super T2> c2,
+            @NotNull Comparator<? super T3> c3,
+            @NotNull Comparator<? super T4> c4,
+            @NotNull Comparator<? super T5> c5
+    ) {
+        return (Comparator<Tuple5<T1, T2, T3, T4, T5>> & Serializable) (t1, t2) -> {
+            final int compare1 = c1.compare(t1._1, t2._1);
+            if (compare1 != 0) {
+                return compare1;
+            }
+            final int compare2 = c2.compare(t1._2, t2._2);
+            if (compare2 != 0) {
+                return compare2;
+            }
+            final int compare3 = c3.compare(t1._3, t2._3);
+            if (compare3 != 0) {
+                return compare3;
+            }
+            final int compare4 = c4.compare(t1._4, t2._4);
+            if (compare4 != 0) {
+                return compare4;
+            }
+            return c5.compare(t1._5, t2._5);
+        };
+    }
+
+    public static <T1, T2, T3, T4, T5, T6> @NotNull Comparator<Tuple6<T1, T2, T3, T4, T5, T6>> comparator(
+            @NotNull Comparator<? super T1> c1,
+            @NotNull Comparator<? super T2> c2,
+            @NotNull Comparator<? super T3> c3,
+            @NotNull Comparator<? super T4> c4,
+            @NotNull Comparator<? super T5> c5,
+            @NotNull Comparator<? super T6> c6
+    ) {
+        return (Comparator<Tuple6<T1, T2, T3, T4, T5, T6>> & Serializable) (t1, t2) -> {
+            final int compare1 = c1.compare(t1._1, t2._1);
+            if (compare1 != 0) {
+                return compare1;
+            }
+            final int compare2 = c2.compare(t1._2, t2._2);
+            if (compare2 != 0) {
+                return compare2;
+            }
+            final int compare3 = c3.compare(t1._3, t2._3);
+            if (compare3 != 0) {
+                return compare3;
+            }
+            final int compare4 = c4.compare(t1._4, t2._4);
+            if (compare4 != 0) {
+                return compare4;
+            }
+            final int compare5 = c5.compare(t1._5, t2._5);
+            if (compare5 != 0) {
+                return compare5;
+            }
+            return c6.compare(t1._6, t2._6);
+        };
+    }
+
+    public static <T1, T2, T3, T4, T5, T6, T7> @NotNull Comparator<Tuple7<T1, T2, T3, T4, T5, T6, T7>> comparator(
+            @NotNull Comparator<? super T1> c1,
+            @NotNull Comparator<? super T2> c2,
+            @NotNull Comparator<? super T3> c3,
+            @NotNull Comparator<? super T4> c4,
+            @NotNull Comparator<? super T5> c5,
+            @NotNull Comparator<? super T6> c6,
+            @NotNull Comparator<? super T7> c7
+    ) {
+        return (Comparator<Tuple7<T1, T2, T3, T4, T5, T6, T7>> & Serializable) (t1, t2) -> {
+            final int compare1 = c1.compare(t1._1, t2._1);
+            if (compare1 != 0) {
+                return compare1;
+            }
+            final int compare2 = c2.compare(t1._2, t2._2);
+            if (compare2 != 0) {
+                return compare2;
+            }
+            final int compare3 = c3.compare(t1._3, t2._3);
+            if (compare3 != 0) {
+                return compare3;
+            }
+            final int compare4 = c4.compare(t1._4, t2._4);
+            if (compare4 != 0) {
+                return compare4;
+            }
+            final int compare5 = c5.compare(t1._5, t2._5);
+            if (compare5 != 0) {
+                return compare5;
+            }
+            final int compare6 = c6.compare(t1._6, t2._6);
+            if (compare6 != 0) {
+                return compare6;
+            }
+            return c7.compare(t1._7, t2._7);
+        };
+    }
+
+    public static <T1, T2, T3, T4, T5, T6, T7, T8> @NotNull Comparator<Tuple8<T1, T2, T3, T4, T5, T6, T7, T8>> comparator(
+            @NotNull Comparator<? super T1> c1,
+            @NotNull Comparator<? super T2> c2,
+            @NotNull Comparator<? super T3> c3,
+            @NotNull Comparator<? super T4> c4,
+            @NotNull Comparator<? super T5> c5,
+            @NotNull Comparator<? super T6> c6,
+            @NotNull Comparator<? super T7> c7,
+            @NotNull Comparator<? super T8> c8
+    ) {
+        return (Comparator<Tuple8<T1, T2, T3, T4, T5, T6, T7, T8>> & Serializable) (t1, t2) -> {
+            final int compare1 = c1.compare(t1._1, t2._1);
+            if (compare1 != 0) {
+                return compare1;
+            }
+            final int compare2 = c2.compare(t1._2, t2._2);
+            if (compare2 != 0) {
+                return compare2;
+            }
+            final int compare3 = c3.compare(t1._3, t2._3);
+            if (compare3 != 0) {
+                return compare3;
+            }
+            final int compare4 = c4.compare(t1._4, t2._4);
+            if (compare4 != 0) {
+                return compare4;
+            }
+            final int compare5 = c5.compare(t1._5, t2._5);
+            if (compare5 != 0) {
+                return compare5;
+            }
+            final int compare6 = c6.compare(t1._6, t2._6);
+            if (compare6 != 0) {
+                return compare6;
+            }
+            final int compare7 = c7.compare(t1._7, t2._7);
+            if (compare7 != 0) {
+                return compare7;
+            }
+            return c8.compare(t1._8, t2._8);
+        };
+    }
+
+    public static <T1, T2, T3, T4, T5, T6, T7, T8, T9> @NotNull Comparator<Tuple9<T1, T2, T3, T4, T5, T6, T7, T8, T9>> comparator(
+            @NotNull Comparator<? super T1> c1,
+            @NotNull Comparator<? super T2> c2,
+            @NotNull Comparator<? super T3> c3,
+            @NotNull Comparator<? super T4> c4,
+            @NotNull Comparator<? super T5> c5,
+            @NotNull Comparator<? super T6> c6,
+            @NotNull Comparator<? super T7> c7,
+            @NotNull Comparator<? super T8> c8,
+            @NotNull Comparator<? super T9> c9
+    ) {
+        return (Comparator<Tuple9<T1, T2, T3, T4, T5, T6, T7, T8, T9>> & Serializable) (t1, t2) -> {
+            final int compare1 = c1.compare(t1._1, t2._1);
+            if (compare1 != 0) {
+                return compare1;
+            }
+            final int compare2 = c2.compare(t1._2, t2._2);
+            if (compare2 != 0) {
+                return compare2;
+            }
+            final int compare3 = c3.compare(t1._3, t2._3);
+            if (compare3 != 0) {
+                return compare3;
+            }
+            final int compare4 = c4.compare(t1._4, t2._4);
+            if (compare4 != 0) {
+                return compare4;
+            }
+            final int compare5 = c5.compare(t1._5, t2._5);
+            if (compare5 != 0) {
+                return compare5;
+            }
+            final int compare6 = c6.compare(t1._6, t2._6);
+            if (compare6 != 0) {
+                return compare6;
+            }
+            final int compare7 = c7.compare(t1._7, t2._7);
+            if (compare7 != 0) {
+                return compare7;
+            }
+            final int compare8 = c8.compare(t1._8, t2._8);
+            if (compare8 != 0) {
+                return compare8;
+            }
+            return c9.compare(t1._9, t2._9);
+        };
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends Tuple> @NotNull Comparator<T> comparator(@NotNull Comparator<?>... comparators) {
+        final int n = comparators.length;
+        switch (n) {
+            case 0:
+                return (Comparator<T>) comparator();
+            case 1:
+                return (Comparator<T>) comparator(comparators[0]);
+            case 2:
+                return (Comparator<T>) comparator(comparators[0], comparators[1]);
+            case 3:
+                return (Comparator<T>) comparator(comparators[0], comparators[1], comparators[2]);
+            case 4:
+                return (Comparator<T>) comparator(comparators[0], comparators[1], comparators[2], comparators[3]);
+            case 5:
+                return (Comparator<T>) comparator(comparators[0], comparators[1], comparators[2], comparators[3], comparators[4]);
+            case 6:
+                return (Comparator<T>) comparator(comparators[0], comparators[1], comparators[2], comparators[3], comparators[4], comparators[5]);
+            case 7:
+                return (Comparator<T>) comparator(comparators[0], comparators[1], comparators[2], comparators[3], comparators[4], comparators[5], comparators[6]);
+            case 8:
+                return (Comparator<T>) comparator(comparators[0], comparators[1], comparators[2], comparators[3], comparators[4], comparators[5], comparators[6], comparators[7]);
+            case 9:
+                return (Comparator<T>) comparator(comparators[0], comparators[1], comparators[2], comparators[3], comparators[4], comparators[5], comparators[6], comparators[7], comparators[8]);
+            default:
+                return (Comparator<T>) new ComparatorN(comparators.clone());
+        }
+    }
+
+    private static final class ComparatorN implements Comparator<Tuple>, Serializable {
+        private static final long serialVersionUID = 6666584328927326964L;
+        private final @NotNull Comparator<?>[] comparators;
+
+        private ComparatorN(@NotNull Comparator<?>[] comparators) {
+            this.comparators = comparators;
+        }
+
+        @Override
+        public int compare(Tuple o1, Tuple o2) {
+            final Comparator<?>[] comparators = this.comparators;
+            final int n = comparators.length;
+            if (o1.arity() != n || o2.arity() != n) {
+                throw new ClassCastException();
+            }
+            for (int i = 0; i < n; i++) {
+                @SuppressWarnings("unchecked") final Comparator<Object> c = (Comparator<Object>) comparators[i];
+                final int res = c.compare(o1.elementAt(i), o2.elementAt(i));
+                if (res != 0) {
+                    return res;
+                }
+            }
+            return 0;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (!(obj instanceof ComparatorN)) {
+                return false;
+            }
+            ComparatorN cn = (ComparatorN) obj;
+            return Arrays.equals(this.comparators, cn.comparators);
+        }
     }
 }
