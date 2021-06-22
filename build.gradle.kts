2a95b750c874c21cd8250e51681a976cf108f522
Glavo <zjx001202@126.com>
2021-06-22 21:25:24 +0800

    release 0.23.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 allprojects {
     group = "org.glavo"
-    version = kalaVersion("0.22.0")
+    version = kalaVersion("0.23.0")
 
     if (this == project(":benchmark")) {
         return@allprojects
