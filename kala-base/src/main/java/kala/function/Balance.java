e9495e616baa7d037923d7243fefa2175c62babc
Glavo <zjx001202@gmail.com>
2022-03-01 01:08:49 +0800

    Balance

---
+++ kala-base/src/main/java/kala/function/Balance.java
@@ -0,0 +1,19 @@
+package kala.function;
+
+import java.util.function.BiPredicate;
+
+@SuppressWarnings("unchecked")
+public interface Balance<T> extends BiPredicate<T, T> {
+    static <T> Balance<T> defaultBalance() {
+        return (Balance<T>) Balances.DEFAULT;
+    }
+
+    static <T> Balance<T> identityBalance() {
+        return (Balance<T>) Balances.IDENTITY;
+    }
+
+
+    int hash(T obj);
+
+    boolean test(T t1, T t2);
+}
---
+++ kala-base/src/main/java/kala/function/Balances.java
@@ -0,0 +1,50 @@
+package kala.function;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+final class Balances {
+    static final Balance<?> DEFAULT = new Default<>();
+    static final Balance<?> IDENTITY = new Identity<>();
+
+    private static final class Default<T> implements Balance<T>, Serializable {
+        private static final long serialVersionUID = 0L;
+
+        @Override
+        public int hash(T obj) {
+            if (obj == null) {
+                return 0;
+            }
+
+            int originalHash = obj.hashCode();
+
+            return originalHash ^ (originalHash >>> 16);
+        }
+
+        @Override
+        public boolean test(T t1, T t2) {
+            return Objects.equals(t1, t2);
+        }
+
+        private Object readResolve() {
+            return DEFAULT;
+        }
+    }
+
+    private static final class Identity<T> implements Balance<T>, Serializable {
+
+        @Override
+        public int hash(T obj) {
+            return System.identityHashCode(obj);
+        }
+
+        @Override
+        public boolean test(T t1, T t2) {
+            return t1 == t2;
+        }
+
+        private Object readResolve() {
+            return IDENTITY;
+        }
+    }
+}
