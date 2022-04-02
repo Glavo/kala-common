dcce9f83c6f95e1a71b102d702984b4d0ac49cf2
Glavo <zjx001202@gmail.com>
2022-04-02 21:58:30 +0800

    close #52: publish to Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,18 @@
 import java.io.RandomAccessFile
+import java.util.Properties
 
 plugins {
     `java-library`
-
+    `maven-publish`
+    signing
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
+
+loadMavenPublishProperties()
 
 allprojects {
     group = "org.glavo.kala"
-    version = kalaVersion("0.35.0")
+    version = "0.35.0" + "-SNAPSHOT"
 
     if (this == project(":benchmark")) {
         return@allprojects
@@ -16,6 +21,7 @@ allprojects {
     apply {
         plugin("java-library")
         plugin("maven-publish")
+        plugin("signing")
     }
 
     repositories {
@@ -54,6 +60,11 @@ allprojects {
         // withJavadocJar()
     }
 
+    val javadocJar = tasks.create<Jar>("javadocJar") {
+        group = "build"
+        archiveClassifier.set("javadoc")
+    }
+
     tasks.withType<Javadoc>().configureEach {
         (options as StandardJavadocDocletOptions).also {
             it.encoding("UTF-8")
@@ -74,8 +85,13 @@ allprojects {
                 version = this@allprojects.version.toString()
                 artifactId = this@allprojects.name
                 from(components["java"])
+                artifact(javadocJar)
 
                 pom {
+                    name.set(project.name)
+                    description.set(project.description)
+                    url.set("https://github.com/Glavo/kala-common")
+
                     licenses {
                         license {
                             name.set("Apache-2.0")
@@ -90,8 +106,23 @@ allprojects {
                             email.set("zjx001202@gmail.com")
                         }
                     }
+
+                    scm {
+                        url.set("https://github.com/Glavo/kala-common")
+                    }
                 }
             }
+        }
+    }
+
+    if (rootProject.ext.has("signing.key")) {
+        signing {
+            useInMemoryPgpKeys(
+                rootProject.ext["signing.keyId"].toString(),
+                rootProject.ext["signing.key"].toString(),
+                rootProject.ext["signing.password"].toString(),
+            )
+            sign(publishing.publications["maven"])
         }
     }
 }
@@ -118,8 +149,45 @@ tasks.test {
     testLogging.showStandardStreams = true
 }
 
-fun kalaVersion(base: String) =
-    if (System.getProperty("kala.release") == "true" || System.getenv("JITPACK") == "true")
-        base
-    else
-        "$base-SNAPSHOT"
+fun loadMavenPublishProperties() {
+    var secretPropsFile = project.rootProject.file("gradle/maven-central-publish.properties")
+    if (!secretPropsFile.exists()) {
+        secretPropsFile =
+            file(System.getProperty("user.home")).resolve(".gradle").resolve("maven-central-publish.properties")
+    }
+
+    if (secretPropsFile.exists()) {
+        // Read local.properties file first if it exists
+        val p = Properties()
+        secretPropsFile.reader().use {
+            p.load(it)
+        }
+
+        p.forEach { (name, value) ->
+            rootProject.ext[name.toString()] = value
+        }
+    }
+
+    listOf(
+        "sonatypeUsername" to "SONATYPE_USERNAME",
+        "sonatypePassword" to "SONATYPE_PASSWORD",
+        "sonatypeStagingProfileId" to "SONATYPE_STAGING_PROFILE_ID",
+        "signing.keyId" to "SIGNING_KEY_ID",
+        "signing.password" to "SIGNING_PASSWORD",
+        "signing.key" to "SIGNING_KEY"
+    ).forEach { (p, e) ->
+        if (!rootProject.ext.has(p)) {
+            rootProject.ext[p] = System.getenv(e)
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            stagingProfileId.set(rootProject.ext["sonatypeStagingProfileId"].toString())
+            username.set(rootProject.ext["sonatypeUsername"].toString())
+            password.set(rootProject.ext["sonatypePassword"].toString())
+        }
+    }
+}
