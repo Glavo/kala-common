c4e783c1dd8f086352df3d2d2e57e87488ba4fca
Glavo <zjx001202@gmail.com>
2022-07-27 17:20:57 +0800

    Release 0.46.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ loadMavenPublishProperties()
 
 allprojects {
     group = "org.glavo.kala"
-    version = "0.46.0" + "-SNAPSHOT"
+    version = "0.46.0"// + "-SNAPSHOT"
 
     description = "Basic components of Kala"
 
