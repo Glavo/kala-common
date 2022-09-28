76d2289bb9d590bab53d2e620e6bed91062bf1ca
Glavo <zjx001202@gmail.com>
2022-09-28 17:25:51 +0800

    fix module-info of kala.collection.primitive

--- kala-collection-primitive/src/main/java/module-info.java
+++ kala-collection-primitive/src/main/java/module-info.java
@@ -1,4 +1,6 @@
 module kala.collection.primitive {
+    requires transitive kala.collection;
+
     exports kala.collection.primitive;
     exports kala.collection.immutable.primitive;
     exports kala.collection.mutable.primitive;
--- kala-jackson/src/main/java/kala/jackson/KalaBaseTypeModifier.java
+++ kala-jackson/src/main/java/kala/jackson/KalaBaseTypeModifier.java
@@ -1,15 +1,23 @@
 package kala.jackson;
 
 import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.type.ReferenceType;
 import com.fasterxml.jackson.databind.type.TypeBindings;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.fasterxml.jackson.databind.type.TypeModifier;
+import kala.control.Option;
 
 import java.lang.reflect.Type;
 
 public class KalaBaseTypeModifier extends TypeModifier {
     @Override
     public JavaType modifyType(JavaType type, Type jdkType, TypeBindings context, TypeFactory typeFactory) {
-        return type; // TODO
+        if (type.isTypeOrSubTypeOf(Option.class)) {
+            if (!(type instanceof ReferenceType)) {
+                return ReferenceType.upgradeFrom(type, type.containedTypeOrUnknown(0));
+            }
+        }
+
+        return type;
     }
 }
