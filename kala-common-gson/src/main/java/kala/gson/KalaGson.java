c88fda6799ddbb8976ae130070489e030a31db5a
Glavo <zjx001202@gmail.com>
2022-12-12 07:24:12 +0800

    KalaGson

--- kala-common-gson/src/main/java/kala/gson/KalaGson.java
+++ kala-common-gson/src/main/java/kala/gson/KalaGson.java
@@ -1,8 +1,32 @@
 package kala.gson;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import kala.gson.base.*;
+import kala.gson.collection.CollectionTypeAdapter;
+
 public final class KalaGson {
     private KalaGson() {
     }
 
+    public static GsonBuilder registerKalaTypeAdapters(GsonBuilder builder) {
+        return registerKalaBaseTypeAdapters(registerKalaCollectionTypeAdapters(builder));
+    }
 
+    public static GsonBuilder registerKalaBaseTypeAdapters(GsonBuilder builder) {
+        return builder
+                .registerTypeAdapterFactory(EitherTypeAdapter.factory())
+                .registerTypeAdapterFactory(OptionTypeAdapter.factory())
+                .registerTypeAdapterFactory(PrimitiveOptionTypeAdapter.factory())
+                .registerTypeAdapterFactory(TupleTypeAdapter.factory())
+                .registerTypeAdapterFactory(PrimitiveTupleTypeAdapter.factory());
+    }
+
+    public static GsonBuilder registerKalaCollectionTypeAdapters(GsonBuilder builder) {
+        return builder.registerTypeAdapterFactory(CollectionTypeAdapter.factory());
+    }
+
+    public static Gson createKalaGson() {
+        return registerKalaTypeAdapters(new GsonBuilder()).create();
+    }
 }
