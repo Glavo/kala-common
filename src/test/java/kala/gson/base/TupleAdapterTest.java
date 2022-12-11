20bf0ad7b44751e3180c2adb17b674bd47574908
Glavo <zjx001202@gmail.com>
2022-12-12 01:59:34 +0800

    PrimitiveTupleAdapter

--- src/test/java/kala/gson/base/TupleAdapterTest.java
+++ src/test/java/kala/gson/base/TupleAdapterTest.java
@@ -4,7 +4,12 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import kala.tuple.*;
+import kala.tuple.primitive.BooleanTuple2;
+import kala.tuple.primitive.IntObjTuple2;
+import kala.tuple.primitive.IntTuple3;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,6 +29,10 @@ public final class TupleAdapterTest {
         assertEquals("[1,2,3,4,5,6,7,8]", gson.toJson(Tuple.of(1, 2, 3, 4, 5, 6, 7, 8)));
         assertEquals("[1,2,3,4,5,6,7,8,9]", gson.toJson(Tuple.of(1, 2, 3, 4, 5, 6, 7, 8, 9)));
         //TODO: assertEquals("[1,2,3,4,5,6,7,8,9,10]", gson.toJson(Tuple.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)));
+
+        assertEquals("[true,true]", gson.toJson(BooleanTuple2.of(true, true)));
+        assertEquals("[10,[0,1,2,3]]", gson.toJson(IntObjTuple2.of(10, List.of(0, 1, 2, 3))));
+        assertEquals("[10,20,30]", gson.toJson(IntTuple3.of(10, 20, 30)));
     }
 
     @Test
@@ -40,5 +49,9 @@ public final class TupleAdapterTest {
         assertEquals(Tuple.of(1, 2, 3, 4, 5, 6, 7, 8), gson.fromJson("[1,2,3,4,5,6,7,8]", new TypeToken<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>>() {}));
         assertEquals(Tuple.of(1, 2, 3, 4, 5, 6, 7, 8, 9), gson.fromJson("[1,2,3,4,5,6,7,8,9]", new TypeToken<Tuple9<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>>() {}));
         assertEquals(Tuple.of(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0), gson.fromJson("[1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0]", new TypeToken<Tuple>() {}));
+
+        assertEquals(BooleanTuple2.of(true, true), gson.fromJson("[true,true]", BooleanTuple2.class));
+        assertEquals(IntObjTuple2.of(10, List.of(0, 1, 2, 3)), gson.fromJson("[10,[0, 1, 2, 3]]", new TypeToken<IntObjTuple2<List<Integer>>>() {}));
+        assertEquals(IntTuple3.of(10, 20, 30), gson.fromJson("[10,20,30]", IntTuple3.class));
     }
 }
