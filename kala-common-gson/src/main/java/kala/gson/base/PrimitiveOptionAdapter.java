854b4326e565be861b3f196e480d4db5a019eec0
Glavo <zjx001202@gmail.com>
2022-12-12 03:13:57 +0800

    PrimitiveOptionAdapter

---
+++ kala-common-gson/src/main/java/kala/gson/base/PrimitiveOptionAdapter.java
@@ -0,0 +1,379 @@
+package kala.gson.base;
+
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.TypeAdapterFactory;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+import kala.control.primitive.*;
+
+import java.io.IOException;
+
+public final class PrimitiveOptionAdapter {
+    private static final TypeAdapterFactory FACTORY = new TypeAdapterFactory() {
+        @Override
+        @SuppressWarnings("unchecked")
+        public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
+            if (PrimitiveOption.class.isAssignableFrom(type.getRawType())) {
+                if (BooleanOption.class.isAssignableFrom(type.getRawType()))
+                    return (TypeAdapter<T>) ForBoolean.INSTANCE;
+                else if (ByteOption.class.isAssignableFrom(type.getRawType()))
+                    return (TypeAdapter<T>) ForByte.INSTANCE;
+                else if (ShortOption.class.isAssignableFrom(type.getRawType()))
+                    return (TypeAdapter<T>) ForShort.INSTANCE;
+                else if (IntOption.class.isAssignableFrom(type.getRawType()))
+                    return (TypeAdapter<T>) ForInt.INSTANCE;
+                else if (LongOption.class.isAssignableFrom(type.getRawType()))
+                    return (TypeAdapter<T>) ForLong.INSTANCE;
+                else if (FloatOption.class.isAssignableFrom(type.getRawType()))
+                    return (TypeAdapter<T>) ForFloat.INSTANCE;
+                else if (DoubleOption.class.isAssignableFrom(type.getRawType()))
+                    return (TypeAdapter<T>) ForDouble.INSTANCE;
+                else if (CharOption.class.isAssignableFrom(type.getRawType()))
+                    return (TypeAdapter<T>) ForChar.INSTANCE;
+                else
+                    return null;
+            }
+            return null;
+        }
+    };
+
+    public static TypeAdapterFactory factory() {
+        return FACTORY;
+    }
+
+    private PrimitiveOptionAdapter() {
+    }
+
+    public static final class ForBoolean extends TypeAdapter<BooleanOption> {
+        public static final ForBoolean INSTANCE = new ForBoolean();
+
+        private ForBoolean() {
+        }
+
+        @Override
+        public void write(JsonWriter out, BooleanOption value) throws IOException {
+            if (value == null) {
+                out.nullValue();
+                return;
+            }
+
+            out.beginArray();
+            if (value.isDefined())
+                out.value(value.get());
+            out.endArray();
+        }
+
+        @Override
+        public BooleanOption read(JsonReader in) throws IOException {
+            if (in.peek() == JsonToken.NULL) {
+                in.nextNull();
+                return null;
+            }
+
+            BooleanOption res;
+
+            in.beginArray();
+            if (in.peek() == JsonToken.END_ARRAY)
+                res = BooleanOption.none();
+            else
+                res = BooleanOption.some(in.nextBoolean());
+            in.endArray();
+
+            return res;
+        }
+    }
+
+    public static final class ForByte extends TypeAdapter<ByteOption> {
+        public static final ForByte INSTANCE = new ForByte();
+
+        private ForByte() {
+        }
+
+        @Override
+        public void write(JsonWriter out, ByteOption value) throws IOException {
+            if (value == null) {
+                out.nullValue();
+                return;
+            }
+
+            out.beginArray();
+            if (value.isDefined())
+                out.value(value.get());
+            out.endArray();
+        }
+
+        @Override
+        public ByteOption read(JsonReader in) throws IOException {
+            if (in.peek() == JsonToken.NULL) {
+                in.nextNull();
+                return null;
+            }
+
+            ByteOption res;
+
+            in.beginArray();
+            if (in.peek() == JsonToken.END_ARRAY)
+                res = ByteOption.none();
+            else {
+                int v = in.nextInt();
+                byte b = (byte) v;
+                if (v != b)
+                    throw new NumberFormatException("Expected an byte but was " + v);
+                res = ByteOption.some(b);
+            }
+            in.endArray();
+
+            return res;
+        }
+    }
+
+    public static final class ForShort extends TypeAdapter<ShortOption> {
+        public static final ForShort INSTANCE = new ForShort();
+
+        private ForShort() {
+        }
+
+        @Override
+        public void write(JsonWriter out, ShortOption value) throws IOException {
+            if (value == null) {
+                out.nullValue();
+                return;
+            }
+
+            out.beginArray();
+            if (value.isDefined())
+                out.value(value.get());
+            out.endArray();
+        }
+
+        @Override
+        public ShortOption read(JsonReader in) throws IOException {
+            if (in.peek() == JsonToken.NULL) {
+                in.nextNull();
+                return null;
+            }
+
+            ShortOption res;
+
+            in.beginArray();
+            if (in.peek() == JsonToken.END_ARRAY)
+                res = ShortOption.none();
+            else {
+                int v = in.nextInt();
+                short s = (short) v;
+                if (v != s)
+                    throw new NumberFormatException("Expected an short but was " + v);
+                res = ShortOption.some(s);
+            }
+            in.endArray();
+
+            return res;
+        }
+    }
+
+    public static final class ForInt extends TypeAdapter<IntOption> {
+        public static final ForInt INSTANCE = new ForInt();
+
+        private ForInt() {
+        }
+
+        @Override
+        public void write(JsonWriter out, IntOption value) throws IOException {
+            if (value == null) {
+                out.nullValue();
+                return;
+            }
+
+            out.beginArray();
+            if (value.isDefined())
+                out.value(value.get());
+            out.endArray();
+        }
+
+        @Override
+        public IntOption read(JsonReader in) throws IOException {
+            if (in.peek() == JsonToken.NULL) {
+                in.nextNull();
+                return null;
+            }
+
+            IntOption res;
+
+            in.beginArray();
+            if (in.peek() == JsonToken.END_ARRAY)
+                res = IntOption.none();
+            else {
+                res = IntOption.some(in.nextInt());
+            }
+            in.endArray();
+
+            return res;
+        }
+    }
+
+    public static final class ForLong extends TypeAdapter<LongOption> {
+        public static final ForLong INSTANCE = new ForLong();
+
+        private ForLong() {
+        }
+
+        @Override
+        public void write(JsonWriter out, LongOption value) throws IOException {
+            if (value == null) {
+                out.nullValue();
+                return;
+            }
+
+            out.beginArray();
+            if (value.isDefined())
+                out.value(value.get());
+            out.endArray();
+        }
+
+        @Override
+        public LongOption read(JsonReader in) throws IOException {
+            if (in.peek() == JsonToken.NULL) {
+                in.nextNull();
+                return null;
+            }
+
+            LongOption res;
+
+            in.beginArray();
+            if (in.peek() == JsonToken.END_ARRAY)
+                res = LongOption.none();
+            else {
+                res = LongOption.some(in.nextLong());
+            }
+            in.endArray();
+
+            return res;
+        }
+    }
+
+    public static final class ForFloat extends TypeAdapter<FloatOption> {
+        public static final ForFloat INSTANCE = new ForFloat();
+
+        private ForFloat() {
+        }
+
+        @Override
+        public void write(JsonWriter out, FloatOption value) throws IOException {
+            if (value == null) {
+                out.nullValue();
+                return;
+            }
+
+            out.beginArray();
+            if (value.isDefined())
+                out.value(value.get());
+            out.endArray();
+        }
+
+        @Override
+        public FloatOption read(JsonReader in) throws IOException {
+            if (in.peek() == JsonToken.NULL) {
+                in.nextNull();
+                return null;
+            }
+
+            FloatOption res;
+
+            in.beginArray();
+            if (in.peek() == JsonToken.END_ARRAY)
+                res = FloatOption.none();
+            else {
+                res = FloatOption.some((float) in.nextDouble());
+            }
+            in.endArray();
+
+            return res;
+        }
+    }
+
+    public static final class ForDouble extends TypeAdapter<DoubleOption> {
+        public static final ForDouble INSTANCE = new ForDouble();
+
+        private ForDouble() {
+        }
+
+        @Override
+        public void write(JsonWriter out, DoubleOption value) throws IOException {
+            if (value == null) {
+                out.nullValue();
+                return;
+            }
+
+            out.beginArray();
+            if (value.isDefined())
+                out.value(value.get());
+            out.endArray();
+        }
+
+        @Override
+        public DoubleOption read(JsonReader in) throws IOException {
+            if (in.peek() == JsonToken.NULL) {
+                in.nextNull();
+                return null;
+            }
+
+            DoubleOption res;
+
+            in.beginArray();
+            if (in.peek() == JsonToken.END_ARRAY)
+                res = DoubleOption.none();
+            else {
+                res = DoubleOption.some(in.nextDouble());
+            }
+            in.endArray();
+
+            return res;
+        }
+    }
+
+    public static final class ForChar extends TypeAdapter<CharOption> {
+        public static final ForChar INSTANCE = new ForChar();
+
+        private ForChar() {
+        }
+
+        @Override
+        public void write(JsonWriter out, CharOption value) throws IOException {
+            if (value == null) {
+                out.nullValue();
+                return;
+            }
+
+            out.beginArray();
+            if (value.isDefined())
+                out.value(String.valueOf(value.get()));
+            out.endArray();
+        }
+
+        @Override
+        public CharOption read(JsonReader in) throws IOException {
+            if (in.peek() == JsonToken.NULL) {
+                in.nextNull();
+                return null;
+            }
+
+            CharOption res;
+
+            in.beginArray();
+            if (in.peek() == JsonToken.END_ARRAY)
+                res = CharOption.none();
+            else {
+                String str = in.nextString();
+                if (str.length() != 1)
+                    throw new NumberFormatException("Expected a char but was " + str);
+                res = CharOption.some(str.charAt(0));
+            }
+            in.endArray();
+
+            return res;
+        }
+    }
+}
--- src/test/java/kala/gson/base/OptionAdapterTest.java
+++ src/test/java/kala/gson/base/OptionAdapterTest.java
@@ -4,16 +4,20 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.reflect.TypeToken;
+import kala.control.AnyOption;
 import kala.control.Option;
+import kala.control.primitive.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class OptionAdapterTest {
-    private final Gson gson = new GsonBuilder().registerTypeAdapterFactory(OptionAdapter.factory()).create();
+    private final Gson gson = new GsonBuilder()
+            .registerTypeAdapterFactory(OptionAdapter.factory())
+            .registerTypeAdapterFactory(PrimitiveOptionAdapter.factory())
+            .create();
 
     @Test
     public void serializeTest() {
@@ -30,12 +34,76 @@ public class OptionAdapterTest {
 
         tree = gson.toJsonTree(Option.none());
         assertEquals(0, tree.getAsJsonArray().size());
+
+        // BooleanOption
+        tree = gson.toJsonTree(BooleanOption.some(true));
+        assertEquals(1, tree.getAsJsonArray().size());
+        assertTrue(tree.getAsJsonArray().get(0).getAsBoolean());
+
+        tree = gson.toJsonTree(BooleanOption.none());
+        assertEquals(0, tree.getAsJsonArray().size());
+
+        // ByteOption
+        tree = gson.toJsonTree(ByteOption.some((byte) 10));
+        assertEquals(1, tree.getAsJsonArray().size());
+        assertEquals(10, tree.getAsJsonArray().get(0).getAsInt());
+
+        tree = gson.toJsonTree(ByteOption.none());
+        assertEquals(0, tree.getAsJsonArray().size());
+
+        // ShortOption
+        tree = gson.toJsonTree(ShortOption.some((short) 10));
+        assertEquals(1, tree.getAsJsonArray().size());
+        assertEquals(10, tree.getAsJsonArray().get(0).getAsInt());
+
+        tree = gson.toJsonTree(ShortOption.none());
+        assertEquals(0, tree.getAsJsonArray().size());
+
+        // IntOption
+        tree = gson.toJsonTree(IntOption.some(10));
+        assertEquals(1, tree.getAsJsonArray().size());
+        assertEquals(10, tree.getAsJsonArray().get(0).getAsInt());
+
+        tree = gson.toJsonTree(IntOption.none());
+        assertEquals(0, tree.getAsJsonArray().size());
+
+        // LongOption
+        tree = gson.toJsonTree(LongOption.some(10));
+        assertEquals(1, tree.getAsJsonArray().size());
+        assertEquals(10, tree.getAsJsonArray().get(0).getAsInt());
+
+        tree = gson.toJsonTree(LongOption.none());
+        assertEquals(0, tree.getAsJsonArray().size());
+
+        // FloatOption
+        tree = gson.toJsonTree(FloatOption.some(10));
+        assertEquals(1, tree.getAsJsonArray().size());
+        assertEquals(10, tree.getAsJsonArray().get(0).getAsFloat());
+
+        tree = gson.toJsonTree(FloatOption.none());
+        assertEquals(0, tree.getAsJsonArray().size());
+
+        // DoubleOption
+        tree = gson.toJsonTree(DoubleOption.some(10));
+        assertEquals(1, tree.getAsJsonArray().size());
+        assertEquals(10, tree.getAsJsonArray().get(0).getAsDouble());
+
+        tree = gson.toJsonTree(DoubleOption.none());
+        assertEquals(0, tree.getAsJsonArray().size());
+
+        // CharOption
+        tree = gson.toJsonTree(CharOption.some('A'));
+        assertEquals(1, tree.getAsJsonArray().size());
+        assertEquals("A", tree.getAsJsonArray().get(0).getAsString());
+
+        tree = gson.toJsonTree(CharOption.none());
+        assertEquals(0, tree.getAsJsonArray().size());
     }
 
     @Test
     public void deserializeTest() {
         var type = new TypeToken<Option<List<Integer>>>() {};
-        Option<List<Integer>> option;
+        AnyOption<?> option;
 
         option = gson.fromJson("null", type);
         assertNull(option);
@@ -45,5 +113,85 @@ public class OptionAdapterTest {
 
         option = gson.fromJson("[]", type);
         assertEquals(Option.none(), option);
+
+        // BooleanOption
+        option = gson.fromJson("null", BooleanOption.class);
+        assertNull(option);
+
+        option = gson.fromJson("[true]", BooleanOption.class);
+        assertEquals(BooleanOption.some(true), option);
+
+        option = gson.fromJson("[]", BooleanOption.class);
+        assertEquals(BooleanOption.none(), option);
+
+        // ByteOption
+        option = gson.fromJson("null", ByteOption.class);
+        assertNull(option);
+
+        option = gson.fromJson("[10]", ByteOption.class);
+        assertEquals(ByteOption.some((byte) 10), option);
+
+        option = gson.fromJson("[]", ByteOption.class);
+        assertEquals(ByteOption.none(), option);
+
+        // ShortOption
+        option = gson.fromJson("null", ShortOption.class);
+        assertNull(option);
+
+        option = gson.fromJson("[10]", ShortOption.class);
+        assertEquals(ShortOption.some((short) 10), option);
+
+        option = gson.fromJson("[]", ShortOption.class);
+        assertEquals(ShortOption.none(), option);
+
+        // IntOption
+        option = gson.fromJson("null", IntOption.class);
+        assertNull(option);
+
+        option = gson.fromJson("[10]", IntOption.class);
+        assertEquals(IntOption.some(10), option);
+
+        option = gson.fromJson("[]", IntOption.class);
+        assertEquals(IntOption.none(), option);
+
+        // LongOption
+        option = gson.fromJson("null", LongOption.class);
+        assertNull(option);
+
+        option = gson.fromJson("[10]", LongOption.class);
+        assertEquals(LongOption.some(10), option);
+
+        option = gson.fromJson("[]", LongOption.class);
+        assertEquals(LongOption.none(), option);
+
+        // FloatOption
+        option = gson.fromJson("null", FloatOption.class);
+        assertNull(option);
+
+        option = gson.fromJson("[10]", FloatOption.class);
+        assertEquals(FloatOption.some(10), option);
+
+        option = gson.fromJson("[]", FloatOption.class);
+        assertEquals(FloatOption.none(), option);
+
+        // DoubleOption
+        option = gson.fromJson("null", DoubleOption.class);
+        assertNull(option);
+
+        option = gson.fromJson("[10]", DoubleOption.class);
+        assertEquals(DoubleOption.some(10), option);
+
+        option = gson.fromJson("[]", DoubleOption.class);
+        assertEquals(DoubleOption.none(), option);
+
+        // CharOption
+        option = gson.fromJson("null", CharOption.class);
+        assertNull(option);
+
+        option = gson.fromJson("[\"A\"]", CharOption.class);
+        assertEquals(CharOption.some('A'), option);
+
+        option = gson.fromJson("[]", CharOption.class);
+        assertEquals(CharOption.none(), option);
     }
 }
