6129afbb95eec5f5fd88f620bcb5925a78d56a0a
Glavo <zjx001202@gmail.com>
2023-02-19 23:51:57 +0800

    Cleanup

--- kala-base/src/main/java/kala/collection/base/Growable.java
+++ kala-base/src/main/java/kala/collection/base/Growable.java
@@ -1,26 +1,10 @@
 package kala.collection.base;
 
 import kala.annotations.Contravariant;
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Objects;
-import java.util.function.Consumer;
 
 @FunctionalInterface
 public interface Growable<@Contravariant T> {
-
-    @ApiStatus.Experimental
-    static <T> @NotNull Growable<T> by(Consumer<? super T> consumer) {
-        Objects.requireNonNull(consumer);
-        return consumer::accept;
-    }
-
-    @ApiStatus.Experimental
-    static <T> @NotNull Growable<T> by(java.util.@NotNull Collection<? super T> list) {
-        Objects.requireNonNull(list);
-        return list::add;
-    }
 
     void plusAssign(T value);
 
