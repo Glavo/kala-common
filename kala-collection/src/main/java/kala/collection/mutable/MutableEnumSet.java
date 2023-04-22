49e21bd449a50ad18104ba57bade4d0f42fe8ea6
Glavo <zjx001202@gmail.com>
2023-04-22 23:03:36 +0800

    Basic impl of MutableEnumSet.OfLargeEnum

--- kala-collection/src/main/java/kala/collection/mutable/MutableEnumSet.java
+++ kala-collection/src/main/java/kala/collection/mutable/MutableEnumSet.java
@@ -4,6 +4,7 @@ import kala.collection.base.AbstractIterator;
 import kala.collection.base.Iterators;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Objects;
@@ -31,10 +32,9 @@ public abstract class MutableEnumSet<E extends Enum<E>> extends AbstractMutableS
         this.constants = (E[]) ENUM_CONSTANTS.get(enumType);
     }
 
-    protected void checkValue(E value) throws ClassCastException, NullPointerException {
-        Class<?> valueType = value.getClass(); //implicit null check of value
-
-        if (!enumType.isAssignableFrom(valueType)) {
+    protected final void checkValue(E value) throws ClassCastException, NullPointerException {
+        Class<?> valueType = value.getDeclaringClass(); //implicit null check of value
+        if (enumType != valueType) {
             throw new ClassCastException("Value " + value + " is not an instance of " + enumType.getName());
         }
     }
@@ -59,38 +59,11 @@ public abstract class MutableEnumSet<E extends Enum<E>> extends AbstractMutableS
         return size;
     }
 
-    private static final class OfEmptyEnum<E extends Enum<E>> extends MutableEnumSet<E> {
-
-        OfEmptyEnum(Class<E> enumType) {
-            super(enumType);
-        }
-
-        @Override
-        public @NotNull Iterator<E> iterator() {
-            return Iterators.empty();
-        }
-
-        @Override
-        public boolean add(E value) {
-            Objects.requireNonNull(value);
-            throw new AssertionError("");
-        }
-
-        @Override
-        public boolean remove(Object value) {
-            return false;
-        }
-
-        @Override
-        public void clear() {
-        }
-    }
-
     private static final class OfSmallEnum<E extends Enum<E>> extends MutableEnumSet<E> {
 
         private long elements;
 
-        private OfSmallEnum(Class<E> enumType) {
+        OfSmallEnum(Class<E> enumType) {
             super(enumType);
         }
 
@@ -199,6 +172,103 @@ public abstract class MutableEnumSet<E extends Enum<E>> extends AbstractMutableS
             public boolean hasNext() {
                 while (current < constants.length) {
                     if ((elements & (1L << current)) != 0) {
+                        return true;
+                    }
+                    current++;
+                }
+                return false;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return constants[current++];
+            }
+        }
+    }
+
+    private static final class OfLargeEnum<E extends Enum<E>> extends MutableEnumSet<E> {
+
+        private final long[] elements;
+
+        private OfLargeEnum(Class<E> enumType) {
+            super(enumType);
+
+            this.elements = new long[(constants.length + Long.SIZE - 1) / Long.SIZE];
+        }
+
+        @Override
+        public @NotNull Iterator<E> iterator() {
+            if (isEmpty()) {
+                return Iterators.empty();
+            }
+
+            return null; // TODO
+        }
+
+        @Override
+        public boolean add(E value) {
+            checkValue(value);
+
+            int ordinal = value.ordinal();
+            int idx = ordinal / Long.SIZE;
+
+            long oldElements = elements[idx];
+            long newElements = oldElements | (1L << (ordinal % Long.SIZE));
+
+            if (newElements != oldElements) {
+                this.size++;
+                this.elements[idx] = newElements;
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public boolean remove(Object value) {
+            if (!enumType.isInstance(value)) {
+                return false;
+            }
+
+            int ordinal = ((E) value).ordinal();
+            int idx = ordinal / Long.SIZE;
+
+            long oldElements = elements[idx];
+            long newElements = oldElements & ~(1L << (ordinal % Long.SIZE));
+
+            if (newElements != oldElements) {
+                this.size--;
+                this.elements[idx] = newElements;
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public void clear() {
+            this.size = 0;
+            Arrays.fill(this.elements, 0L);
+        }
+
+        private static final class Itr<E> extends AbstractIterator<E> {
+            private final E[] constants;
+            private final long[] elements;
+
+            private int current = 0;
+
+            private Itr(E[] constants, long[] elements) {
+                this.constants = constants;
+                this.elements = elements;
+            }
+
+            @Override
+            public boolean hasNext() {
+                while (current < constants.length) {
+                    if ((elements[current / Long.SIZE] & (1L << (current % Long.SIZE))) != 0) {
                         return true;
                     }
                     current++;
