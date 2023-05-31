7db5662f82af30cf92de20d0d3d15d73b514a955
Glavo <zjx001202@gmail.com>
2023-06-01 03:59:02 +0800

    update BitArray

--- kala-collection-primitive/src/main/java/kala/collection/primitive/BitArray.java
+++ kala-collection-primitive/src/main/java/kala/collection/primitive/BitArray.java
@@ -1,47 +1,15 @@
 package kala.collection.primitive;
 
 import kala.Conditions;
-import kala.collection.base.primitive.AbstractBooleanIterator;
+import kala.collection.base.primitive.BitArrays;
 import kala.collection.base.primitive.BooleanIterator;
 import kala.collection.base.primitive.LongArrays;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 public class BitArray extends AbstractBooleanSeq implements IndexedBooleanSeq, Serializable {
     private static final long serialVersionUID = 4673372256434484L;
-
-    //region Bit Array Utils
-
-    public static boolean get(long bits, int position) {
-        return ((bits >> position) & 1) != 0;
-    }
-
-    public static long set(long bits, int position, boolean newValue) {
-        if (newValue) {
-            return bits | (1L << position);
-        } else {
-            return bits & -(1L << position);
-        }
-    }
-
-    public static boolean get(long[] bitsArray, int index) {
-        return get(bitsArray[index / Long.SIZE], index % Long.SIZE);
-    }
-
-    public static void set(long[] bitsArray, int index, boolean newValue) {
-        int i = index / Long.SIZE;
-        bitsArray[i] = set(bitsArray[i], index % Long.SIZE, newValue);
-    }
-
-    public static BooleanIterator iterator(long[] bitsArray, int beginIndex, int endIndex) {
-        Objects.requireNonNull(bitsArray);
-        Conditions.checkPositionIndices(beginIndex, endIndex, bitsArray.length * Long.SIZE);
-        return new Itr(bitsArray, beginIndex, endIndex);
-    }
-
-    //endregion
 
     private static final BitArray EMPTY = new BitArray(LongArrays.EMPTY, 0);
 
@@ -62,11 +30,11 @@ public class BitArray extends AbstractBooleanSeq implements IndexedBooleanSeq, S
 
     //endregion
 
-    protected final long[] bitArray;
+    protected final long[] bitsArray;
     protected final int size;
 
-    protected BitArray(long[] bitArray, int size) {
-        this.bitArray = bitArray;
+    protected BitArray(long[] bitsArray, int size) {
+        this.bitsArray = bitsArray;
         this.size = size;
     }
 
@@ -77,7 +45,7 @@ public class BitArray extends AbstractBooleanSeq implements IndexedBooleanSeq, S
 
     @Override
     public @NotNull BooleanIterator iterator() {
-        return new Itr(bitArray, 0, size);
+        return BitArrays.iterator(bitsArray, 0, size);
     }
 
     @Override
@@ -88,42 +56,6 @@ public class BitArray extends AbstractBooleanSeq implements IndexedBooleanSeq, S
     @Override
     public boolean get(int index) {
         Conditions.checkElementIndex(index, size);
-        return get(bitArray, index);
-    }
-
-    private static final class Itr extends AbstractBooleanIterator {
-        private final int endIndex;
-        private final long[] bitsArray;
-
-        private long bits;
-        private int index;
-
-        Itr(long[] bitsArray, int index, int endIndex) {
-            this.bitsArray = bitsArray;
-            this.index = index;
-            this.endIndex = endIndex;
-
-            if (index % Long.SIZE != 0) {
-                bits = bitsArray[index / Long.SIZE];
-            }
-        }
-
-        @Override
-        public boolean hasNext() {
-            return index < endIndex;
-        }
-
-        @Override
-        public boolean nextBoolean() {
-            checkStatus();
-
-            int position = index % Long.SIZE;
-
-            if (position == 0)
-                bits = bitsArray[index / Long.SIZE];
-
-            index++;
-            return get(bits, position);
-        }
+        return BitArrays.get(bitsArray, index);
     }
 }
