b20446d55107d99f8e679d952474d30b191a36e3
Glavo <zjx001202@gmail.com>
2024-05-06 18:28:16 +0800

    Convert Either to sealed interface

--- kala-base/src/main/java/kala/control/Either.java
+++ kala-base/src/main/java/kala/control/Either.java
@@ -20,7 +20,6 @@ import kala.annotations.UnstableName;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.Serial;
 import java.io.Serializable;
 import java.util.NoSuchElementException;
 import java.util.Objects;
@@ -29,37 +28,34 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 @SuppressWarnings("unchecked")
-public abstract sealed class Either<@Covariant A, @Covariant B> implements Serializable {
-    Either() {
-    }
-
+public sealed interface Either<@Covariant A, @Covariant B> extends Serializable permits Either.Left, Either.Right {
     @Contract(value = "_ -> param1", pure = true)
-    public static <A, B> Either<A, B> narrow(Either<? extends A, ? extends B> either) {
+    static <A, B> Either<A, B> narrow(Either<? extends A, ? extends B> either) {
         return (Either<A, B>) either;
     }
 
     @Contract(value = "_ -> param1", pure = true)
-    public static <A, B> Either.Left<A, B> narrow(Either.Left<? extends A, ? extends B> left) {
+    static <A, B> Either.Left<A, B> narrow(Either.Left<? extends A, ? extends B> left) {
         return (Either.Left<A, B>) left;
     }
 
     @Contract(value = "_ -> param1", pure = true)
-    public static <A, B> Either.Right<A, B> narrow(Either.Right<? extends A, ? extends B> right) {
+    static <A, B> Either.Right<A, B> narrow(Either.Right<? extends A, ? extends B> right) {
         return (Either.Right<A, B>) right;
     }
 
     @Contract("_ -> new")
-    public static <A, B> Either.@NotNull Left<A, B> left(A value) {
+    static <A, B> Either.@NotNull Left<A, B> left(A value) {
         return new Left<>(value);
     }
 
     @Contract("_ -> new")
-    public static <A, B> Either.@NotNull Right<A, B> right(B value) {
+    static <A, B> Either.@NotNull Right<A, B> right(B value) {
         return new Right<>(value);
     }
 
     @UnstableName
-    public static <T> T join(@NotNull Either<? extends T, ? extends T> either) {
+    static <T> T join(@NotNull Either<? extends T, ? extends T> either) {
         if (either.isLeft()) {
             return either.getLeftValue();
         } else {
@@ -67,207 +63,120 @@ public abstract sealed class Either<@Covariant A, @Covariant B> implements Seria
         }
     }
 
-    public abstract boolean isLeft();
+    default boolean isLeft() {
+        return this instanceof Either.Left;
+    }
 
-    public abstract boolean isRight();
+    default boolean isRight() {
+        return this instanceof Either.Right;
+    }
 
-    public abstract A getLeftValue();
+    default A getLeftValue() {
+        if (this instanceof Either.Left(var value)) {
+            return value;
+        } else {
+            throw new NoSuchElementException("Either.Right#getLeftValue");
+        }
+    }
 
-    public abstract @NotNull Option<A> getLeftOption();
+    default @NotNull Option<A> getLeftOption() {
+        return this instanceof Left(var value) ? Option.some(value) : Option.none();
+    }
 
-    public abstract B getRightValue();
+    default B getRightValue() {
+        if (this instanceof Either.Right(var value)) {
+            return value;
+        } else {
+            throw new NoSuchElementException("Either.Right#getLeftValue");
+        }
+    }
 
-    public abstract @NotNull Option<B> getRightOption();
+    default @NotNull Option<B> getRightOption() {
+        return this instanceof Right(var value) ? Option.some(value) : Option.none();
+    }
 
-    public abstract <U, V> @NotNull Either<U, V> map(
+    default <U, V> @NotNull Either<U, V> map(
             @NotNull Function<? super A, ? extends U> leftMapper,
             @NotNull Function<? super B, ? extends V> rightMapper
-    );
+    ) {
+        return switch (this) {
+            case Left(var value) -> Either.left(leftMapper.apply(value));
+            case Right(var value) -> Either.right(rightMapper.apply(value));
+        };
+    }
 
-    public abstract <U> @NotNull Either<U, B> mapLeft(@NotNull Function<? super A, ? extends U> mapper);
+    default <U> @NotNull Either<U, B> mapLeft(@NotNull Function<? super A, ? extends U> mapper) {
+        return this instanceof Left(var value) ? Either.left(mapper.apply(value)) : (Either<U, B>) this;
+    }
 
-    public abstract <U> @NotNull Either<A, U> mapRight(@NotNull Function<? super B, ? extends U> mapper);
+    default <U> @NotNull Either<A, U> mapRight(@NotNull Function<? super B, ? extends U> mapper) {
+        return this instanceof Right(var value) ? Either.right(mapper.apply(value)) : (Either<A, U>) this;
+    }
 
-    public abstract <U> U fold(
+    default <U> U fold(
             @NotNull Function<? super A, ? extends U> leftMapper,
             @NotNull Function<? super B, ? extends U> rightMapper
-    );
+    ) {
+        return switch (this) {
+            case Left(var value) -> leftMapper.apply(value);
+            case Right(var value) -> rightMapper.apply(value);
+        };
+    }
 
-    public abstract <C, D, U> U bifold(
+    default <C, D, U> U bifold(
             @NotNull Either<? extends C, ? extends D> other,
             U defaultValue,
             @NotNull BiFunction<? super A, ? super C, ? extends U> leftMapper,
             @NotNull BiFunction<? super B, ? super D, ? extends U> rightMapper
-    );
+    ) {
+        if (this instanceof Either.Left(var value1) && other instanceof Either.Left(var value2)) {
+            return leftMapper.apply(value1, value2);
+        }
+
+        if (this instanceof Either.Right(var value1) && other instanceof Either.Right(var value2)) {
+            return rightMapper.apply(value1, value2);
+        }
+
+        return defaultValue;
+    }
 
     @Contract("-> new")
-    public abstract @NotNull Either<B, A> swap();
+    default @NotNull Either<B, A> swap() {
+        return switch (this) {
+            case Left(var value) -> Either.right(value);
+            case Right(var value) -> Either.left(value);
+        };
+    }
 
     @Contract("-> new")
-    public abstract @NotNull Result<B, A> toResult();
-
-    @Contract(" -> new")
-    public final @NotNull LeftProjection leftProjection() {
-        return this.new LeftProjection();
+    default @NotNull Result<B, A> toResult() {
+        return switch (this) {
+            case Left(var value) -> Result.err(value);
+            case Right(var value) -> Result.ok(value);
+        };
     }
 
     @Contract(" -> new")
-    public final @NotNull RightProjection rightProjection() {
-        return this.new RightProjection();
+    default @NotNull LeftProjection<A, B> leftProjection() {
+        return new LeftProjection<>(this);
     }
 
-    public abstract void forEach(
+    @Contract(" -> new")
+    default @NotNull RightProjection<A, B> rightProjection() {
+        return new RightProjection<>(this);
+    }
+
+    default void forEach(
             @NotNull Consumer<? super A> leftConsumer,
             @NotNull Consumer<? super B> rightConsumer
-    );
-
-    public final static class Left<@Covariant A, @Covariant B> extends Either<A, B> {
-        @Serial
-        private static final long serialVersionUID = -1160729620210301179L;
-
-        private static final int HASH_MAGIC = -1951578063;
-
-        private final A value;
-
-        Left(A value) {
-            this.value = value;
+    ) {
+        switch (this) {
+            case Left(var value) -> leftConsumer.accept(value);
+            case Right(var value) -> rightConsumer.accept(value);
         }
+    }
 
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public boolean isLeft() {
-            return true;
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public boolean isRight() {
-            return false;
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public A getLeftValue() {
-            return value;
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public @NotNull Option<A> getLeftOption() {
-            return Option.some(value);
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        @Contract("-> fail")
-        public B getRightValue() {
-            throw new NoSuchElementException("Either.Left.getRightValue");
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public @NotNull Option<B> getRightOption() {
-            return Option.none();
-        }
-
-        @Override
-        public <U, V> @NotNull Either<U, V> map(
-                @NotNull Function<? super A, ? extends U> leftMapper,
-                @NotNull Function<? super B, ? extends V> rightMapper) {
-            return Either.left(leftMapper.apply(value));
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public <U> @NotNull Either<U, B> mapLeft(@NotNull Function<? super A, ? extends U> mapper) {
-            Objects.requireNonNull(mapper);
-            return Either.left(mapper.apply(value));
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public <U> @NotNull Either<A, U> mapRight(@NotNull Function<? super B, ? extends U> mapper) {
-            return (Either<A, U>) this;
-        }
-
-        @Override
-        public <U> U fold(
-                @NotNull Function<? super A, ? extends U> leftMapper,
-                @NotNull Function<? super B, ? extends U> rightMapper) {
-            return leftMapper.apply(value);
-        }
-
-        @Override
-        public <C, D, U> U bifold(
-                @NotNull Either<? extends C, ? extends D> other,
-                U defaultValue,
-                @NotNull BiFunction<? super A, ? super C, ? extends U> leftMapper,
-                @NotNull BiFunction<? super B, ? super D, ? extends U> rightMapper) {
-
-            if (other instanceof Left) {
-                return leftMapper.apply(this.value, ((Left<? extends C, ? extends D>) other).value);
-            } else {
-                return defaultValue;
-            }
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public @NotNull Either<B, A> swap() {
-            return Either.right(value);
-        }
-
-        @Override
-        public @NotNull Result<B, A> toResult() {
-            return Result.err(value);
-        }
-
-        @Override
-        public void forEach(@NotNull Consumer<? super A> leftConsumer, @NotNull Consumer<? super B> rightConsumer) {
-            leftConsumer.accept(value);
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) {
-                return true;
-            }
-            if (!(obj instanceof Left<?, ?>)) {
-                return false;
-            }
-
-            return Objects.equals(value, ((Left<?, ?>) obj).value);
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public int hashCode() {
-            return Objects.hashCode(value) + HASH_MAGIC;
-        }
-
+    record Left<@Covariant A, @Covariant B>(A value) implements Either<A, B> {
         /**
          * {@inheritDoc}
          */
@@ -277,154 +186,7 @@ public abstract sealed class Either<@Covariant A, @Covariant B> implements Seria
         }
     }
 
-    public final static class Right<@Covariant A, @Covariant B> extends Either<A, B> {
-        @Serial
-        private static final long serialVersionUID = -3372589401685464421L;
-
-        private static final int HASH_MAGIC = 1973604283;
-
-        private final B value;
-
-        Right(B value) {
-            this.value = value;
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public boolean isLeft() {
-            return false;
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public boolean isRight() {
-            return true;
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        @Contract("-> fail")
-        public A getLeftValue() {
-            throw new NoSuchElementException("Either.Right.getLeftValue");
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public @NotNull Option<A> getLeftOption() {
-            return Option.none();
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public B getRightValue() {
-            return value;
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public @NotNull Option<B> getRightOption() {
-            return Option.some(value);
-        }
-
-        @Override
-        public <U, V> @NotNull Either<U, V> map(
-                @NotNull Function<? super A, ? extends U> leftMapper,
-                @NotNull Function<? super B, ? extends V> rightMapper
-        ) {
-            return Either.right(rightMapper.apply(value));
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public <U> @NotNull Either<U, B> mapLeft(@NotNull Function<? super A, ? extends U> mapper) {
-            return (Either<U, B>) this;
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public <U> @NotNull Either<A, U> mapRight(@NotNull Function<? super B, ? extends U> mapper) {
-            Objects.requireNonNull(mapper);
-            return Either.right(mapper.apply(value));
-        }
-
-        @Override
-        public <U> U fold(
-                @NotNull Function<? super A, ? extends U> leftMapper,
-                @NotNull Function<? super B, ? extends U> rightMapper) {
-            return rightMapper.apply(value);
-        }
-
-        @Override
-        public <C, D, U> U bifold(
-                @NotNull Either<? extends C, ? extends D> other,
-                U defaultValue,
-                @NotNull BiFunction<? super A, ? super C, ? extends U> leftMapper,
-                @NotNull BiFunction<? super B, ? super D, ? extends U> rightMapper) {
-
-            if (other instanceof Right) {
-                return rightMapper.apply(this.value, ((Right<? extends C, ? extends D>) other).value);
-            } else {
-                return defaultValue;
-            }
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public @NotNull Either<B, A> swap() {
-            return Either.left(value);
-        }
-
-        @Override
-        public @NotNull Result<B, A> toResult() {
-            return Result.ok(value);
-        }
-
-        @Override
-        public void forEach(@NotNull Consumer<? super A> leftConsumer, @NotNull Consumer<? super B> rightConsumer) {
-            rightConsumer.accept(value);
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) {
-                return true;
-            }
-            if (!(obj instanceof Right<?, ?>)) {
-                return false;
-            }
-
-            return Objects.equals(value, ((Right<?, ?>) obj).value);
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public int hashCode() {
-            return Objects.hashCode(value) + HASH_MAGIC;
-        }
-
+    record Right<@Covariant A, @Covariant B>(B value) implements Either<A, B> {
         /**
          * {@inheritDoc}
          */
@@ -434,53 +196,34 @@ public abstract sealed class Either<@Covariant A, @Covariant B> implements Seria
         }
     }
 
-    public abstract sealed class Projection<@Covariant T> implements OptionContainer<T> {
-        Projection() {
-        }
-
-        private static final int HASH_MAGIC = 905770825;
-
-        public final @NotNull Either<A, B> getEither() {
-            return Either.this;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-
-            return Either.this.equals(((Projection<?>) o).getEither());
-        }
-
-        @Override
-        public final int hashCode() {
-            return Either.this.hashCode() + HASH_MAGIC;
-        }
+    sealed interface Projection<@Covariant T, A, B> extends OptionContainer<T> permits LeftProjection, RightProjection {
+        @NotNull
+        Either<A, B> getEither();
     }
 
-    public final class LeftProjection extends Projection<A> {
+    record LeftProjection<A, B>(Either<A, B> either) implements Projection<A, A, B> {
+        @Override
+        public @NotNull Either<A, B> getEither() {
+            return either;
+        }
+
         @Override
         public boolean isDefined() {
-            return Either.this.isLeft();
+            return either.isLeft();
         }
 
         @Override
         public A get() {
-            return Either.this.getLeftValue();
+            return either.getLeftValue();
         }
 
         @Override
-        @NotNull
-        public <U> Either<U, B>.LeftProjection map(@NotNull Function<? super A, ? extends U> mapper) {
+        public <U> @NotNull LeftProjection<U, B> map(@NotNull Function<? super A, ? extends U> mapper) {
             Objects.requireNonNull(mapper);
             if (isDefined()) {
-                return Either.<U, B>left(mapper.apply(getLeftValue())).new LeftProjection();
+                return new LeftProjection<>(Either.left(mapper.apply(either.getLeftValue())));
             }
-            return (Either<U, B>.LeftProjection) this;
+            return (LeftProjection<U, B>) this;
         }
 
         /**
@@ -489,38 +232,42 @@ public abstract sealed class Either<@Covariant A, @Covariant B> implements Seria
         @NotNull
         @Override
         public String toString() {
-            return Either.this + ".LeftProjection";
+            return either + ".LeftProjection";
         }
     }
 
-    public final class RightProjection extends Projection<B> {
+    record RightProjection<A, B>(Either<A, B> either) implements Projection<B, A, B> {
+
+        @Override
+        public @NotNull Either<A, B> getEither() {
+            return either;
+        }
+
         @Override
         public boolean isDefined() {
-            return Either.this.isRight();
+            return either.isRight();
         }
 
         @Override
         public B get() {
-            return Either.this.getRightValue();
+            return either.getRightValue();
         }
 
         @Override
-        @NotNull
-        public <U> Either<A, U>.RightProjection map(@NotNull Function<? super B, ? extends U> mapper) {
+        public <U> @NotNull RightProjection<A, U> map(@NotNull Function<? super B, ? extends U> mapper) {
             Objects.requireNonNull(mapper);
             if (isDefined()) {
-                return Either.<A, U>right(mapper.apply(getRightValue())).new RightProjection();
+                return new RightProjection<>(Either.right(mapper.apply(either.getRightValue())));
             }
-            return (Either<A, U>.RightProjection) this;
+            return (RightProjection<A, U>) this;
         }
 
         /**
          * {@inheritDoc}
          */
-        @NotNull
         @Override
-        public String toString() {
-            return Either.this + ".RightProjection";
+        public @NotNull String toString() {
+            return either + ".RightProjection";
         }
     }
 }
