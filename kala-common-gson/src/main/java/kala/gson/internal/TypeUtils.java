a6d94fbe89606c67dbbc921259c885da41db901b
Glavo <zjx001202@gmail.com>
2024-05-17 21:43:42 +0800

    Update kala.gson

--- kala-common-gson/src/main/java/kala/gson/internal/TypeUtils.java
+++ kala-common-gson/src/main/java/kala/gson/internal/TypeUtils.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (C) 2008 Google Inc.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package kala.gson.internal;
 
 import static java.util.Objects.requireNonNull;
