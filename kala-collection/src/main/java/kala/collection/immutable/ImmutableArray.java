06425760e1b57ca3813a02cd8e13eac7cd49ab7b
Glavo <zjx001202@gmail.com>
2024-05-23 08:16:12 +0800

    Update ImmutableArray

--- kala-collection/src/main/java/kala/collection/immutable/ImmutableArray.java
+++ kala-collection/src/main/java/kala/collection/immutable/ImmutableArray.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024 Glavo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package kala.collection.immutable;
 
 import kala.collection.*;
@@ -15,6 +30,7 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.*;
 import java.util.function.*;
@@ -24,6 +40,7 @@ import static kala.Conditions.checkPositionIndices;
 
 @SuppressWarnings("unchecked")
 public final class ImmutableArray<@Covariant E> extends ArraySeq<E> implements ImmutableSeq<E>, Serializable {
+    @Serial
     private static final long serialVersionUID = 1845940935381169058L;
 
     public static final ImmutableArray<?> EMPTY = new ImmutableArray<>(GenericArrays.EMPTY_OBJECT_ARRAY);
