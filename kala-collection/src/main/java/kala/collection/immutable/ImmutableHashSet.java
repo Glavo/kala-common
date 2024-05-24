d21d751eac751a66cd8265f289320dcbab4615af
Glavo <zjx001202@gmail.com>
2024-05-24 18:20:18 +0800

    Update ImmutableHashSet

--- kala-collection/src/main/java/kala/collection/immutable/ImmutableHashSet.java
+++ kala-collection/src/main/java/kala/collection/immutable/ImmutableHashSet.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024 Glavo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package kala.collection.immutable;
 
 import kala.collection.factory.CollectionFactory;
@@ -12,7 +27,8 @@ import java.util.Iterator;
 import java.util.stream.Stream;
 
 @SuppressWarnings("unchecked")
-public class ImmutableHashSet<E> extends FromJavaConvert.SetFromJava<E> implements ImmutableSet<E>, Serializable {
+public final class ImmutableHashSet<E> extends FromJavaConvert.SetFromJava<E> implements ImmutableSet<E>, Serializable {
+    @Serial
     private static final long serialVersionUID = 9051623197618851317L;
 
     private static final Factory<?> FACTORY = new Factory<>();
@@ -144,6 +160,7 @@ public class ImmutableHashSet<E> extends FromJavaConvert.SetFromJava<E> implemen
         return factory();
     }
 
+    @Serial
     private Object writeReplace() {
         return new SerializationWrapper<>(this);
     }
@@ -231,6 +248,7 @@ public class ImmutableHashSet<E> extends FromJavaConvert.SetFromJava<E> implemen
             value = set.isEmpty() ? ImmutableHashSet.empty() : new ImmutableHashSet<>(set);
         }
 
+        @Serial
         private Object readResolve() {
             return value;
         }
