82da2518b8b30506e933ea83d19b437a5a5a8d69
Glavo <zjx001202@gmail.com>
2024-06-13 23:59:32 +0800

    update ResultTest

--- src/test/java/kala/control/ResultTest.java
+++ src/test/java/kala/control/ResultTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024 Glavo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package kala.control;
 
 import kala.SerializationUtils;
@@ -5,13 +20,14 @@ import org.junit.jupiter.api.*;
 
 import java.io.IOException;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ResultTest {
 
-    Result<String, ?> ok = Result.ok("value");
-    Result<?, Integer> err = Result.err(10);
+    private static final Result<String, ?> ok = Result.ok("value");
+    private static final Result<?, Integer> err = Result.err(10);
 
     @Test
     public void factoryTest() {
@@ -36,6 +52,18 @@ public class ResultTest {
 
         assertSame(err, err.map(it -> null));
         assertEquals(Result.err("10"), err.mapErr(String::valueOf));
+    }
+
+    @Test
+    public void flatMapTest() {
+        assertEquals(Result.ok(5), ok.flatMap(it -> Result.ok(it.length())));
+        // assertEquals(Result.err(5), ok.flatMap(it -> (Result<String, Integer>) Result.err(it.length())));
+    }
+
+    @Test
+    public void toEitherTest() {
+        assertEquals(Either.right("value"), ok.toEither());
+        assertEquals(Either.left(10), err.toEither());
     }
 
     @Test
