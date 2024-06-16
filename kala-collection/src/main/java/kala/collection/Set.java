769185b7f16edfa3ca63ffb58abf9fcba1d1ee53
Glavo <zjx001202@gmail.com>
2024-06-17 03:17:02 +0800

    fix Set

--- kala-collection/src/main/java/kala/collection/Set.java
+++ kala-collection/src/main/java/kala/collection/Set.java
@@ -1,15 +1,28 @@
+/*
+ * Copyright 2024 Glavo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package kala.collection;
 
 import kala.collection.base.Iterators;
 import kala.collection.factory.CollectionFactory;
-import kala.collection.immutable.ImmutableHashSet;
 import kala.collection.immutable.ImmutableSet;
 import kala.collection.internal.convert.AsJavaConvert;
 import kala.collection.internal.convert.FromJavaConvert;
 import kala.collection.internal.view.SetViews;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.Serializable;
 import java.util.Iterator;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
@@ -54,6 +67,7 @@ public interface Set<E> extends Collection<E>, SetLike<E>, AnySet<E> {
         return ImmutableSet.of(value1, value2, value3, value4, value5);
     }
 
+    @SafeVarargs
     static <E> @NotNull Set<E> of(E... values) {
         return ImmutableSet.of(values);
     }
