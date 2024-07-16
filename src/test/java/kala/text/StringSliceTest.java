86c65f17b2b4e206d9cd4da70f8c22f63eb5e38a
Glavo <zjx001202@gmail.com>
2024-07-16 21:26:06 +0800

    Update StringSliceTest

--- src/test/java/kala/text/StringSliceTest.java
+++ src/test/java/kala/text/StringSliceTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024 Glavo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package kala.text;
 
 import org.junit.jupiter.api.*;
@@ -5,6 +20,11 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StringSliceTest {
+
+    private static void assertSliceEquals(String expected, StringSlice actual) {
+        assertEquals(StringSlice.of(expected), actual);
+    }
+
     @Test
     void containsTest() {
         StringSlice slice = StringSlice.of("abc");
@@ -25,5 +45,15 @@ public class StringSliceTest {
         assertFalse(slice.contains("bc"));
         assertFalse(slice.contains("abc"));
         assertFalse(slice.contains("ac"));
+    }
+
+    @Test
+    void substringTest() {
+        StringSlice slice = StringSlice.of("abc");
+        assertSliceEquals("abc", slice.substring(0));
+        assertSliceEquals("bc", slice.substring(1));
+        assertSliceEquals("c", slice.substring(2));
+        assertSliceEquals("", slice.substring(3));
+        assertThrows(IndexOutOfBoundsException.class, () -> slice.substring(4));
     }
 }
