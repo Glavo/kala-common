89593cb844ec26252f82d668d7323d5c96edb9e7
Glavo <zjx001202@gmail.com>
2024-07-18 20:49:35 +0800

    Update TypeLiteralTest

--- src/test/java/kala/reflect/TypeLiteralTest.java
+++ src/test/java/kala/reflect/TypeLiteralTest.java
@@ -1,15 +1,26 @@
+/*
+ * Copyright 2024 Glavo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package kala.reflect;
 
-import kala.SerializationUtils;
 import kala.control.Option;
 import org.junit.jupiter.api.*;
 
-import java.io.IOException;
 import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
