ff5e42f196709248417d92c78b3deee01cc13511
Glavo <zjx001202@gmail.com>
2025-01-04 21:27:23 +0800

    Update MutableArrayDeque

--- kala-collection/src/main/java/kala/collection/mutable/MutableArrayDeque.java
+++ kala-collection/src/main/java/kala/collection/mutable/MutableArrayDeque.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2025 Glavo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package kala.collection.mutable;
 
 import kala.Conditions;
@@ -11,6 +26,7 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -23,6 +39,7 @@ import java.util.stream.Stream;
 
 @SuppressWarnings("unchecked")
 public final class MutableArrayDeque<E> extends AbstractMutableList<E> implements MutableDeque<E>, IndexedSeq<E>, Serializable {
+    @Serial
     private static final long serialVersionUID = -4166302067142375121L;
 
     private static final MutableListFactory<Object, MutableArrayDeque<Object>> FACTORY = MutableArrayDeque::new;
