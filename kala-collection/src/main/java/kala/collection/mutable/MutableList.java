a97c9b8d2ba3ae2600138d10bedbf3ad4def3686
Glavo <zjx001202@gmail.com>
2025-01-04 21:34:41 +0800

    Update MutableList

--- kala-collection/src/main/java/kala/collection/mutable/MutableList.java
+++ kala-collection/src/main/java/kala/collection/mutable/MutableList.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2025 Glavo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package kala.collection.mutable;
 
 import kala.Conditions;
@@ -331,8 +346,10 @@ public interface MutableList<E> extends MutableSeq<E>, Growable<E> {
         int rangeLength = endIndex - beginIndex;
 
         if (rangeLength == 0) {
-            // do nothing
-        } else if (rangeLength == size) {
+            return;
+        }
+
+        if (rangeLength == size) {
             clear();
         } else {
             for (int i = 0; i < rangeLength; i++) {
