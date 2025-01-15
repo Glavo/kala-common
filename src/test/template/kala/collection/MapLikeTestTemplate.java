e4c747ca85a34a4434376e29e40fbd5b39c94d73
Glavo <zjx001202@gmail.com>
2025-01-15 13:14:59 +0800

    Create MapLikeTestTemplate

--- src/test/template/kala/collection/MapLikeTestTemplate.java
+++ src/test/template/kala/collection/MapLikeTestTemplate.java
@@ -15,7 +15,14 @@
  */
 package kala.collection;
 
-public interface MapTestTemplate extends MapLikeTestTemplate {
+public interface MapLikeTestTemplate {
+    default Integer[][] data1() {
+        return TestData.data1;
+    }
+
+    default String[][] data1s() {
+        return TestData.data1s;
+    }
 
 
 }
