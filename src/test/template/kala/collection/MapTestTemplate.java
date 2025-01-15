4923140daf19a367ecddef5890b7810a3af7e072
Glavo <zjx001202@gmail.com>
2025-01-15 17:13:57 +0800

    Add more tests for Map

--- src/test/template/kala/collection/MapTestTemplate.java
+++ src/test/template/kala/collection/MapTestTemplate.java
@@ -16,15 +16,143 @@
 package kala.collection;
 
 import kala.collection.factory.MapFactory;
+import kala.tuple.Tuple;
 import kala.tuple.Tuple2;
+import org.junit.jupiter.api.Test;
 
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@SuppressWarnings("unchecked")
 public interface MapTestTemplate extends MapLikeTestTemplate {
     @Override
     <K, V> MapFactory<K, V, ?, ? extends Map<K, V>> factory();
 
     @Override
-    @SuppressWarnings("unchecked")
     default <K, V> Map<K, V> ofEntries(Tuple2<K, V>... tuples) {
         return this.<K, V>factory().ofEntries(tuples);
+    }
+
+    default Class<?> mapType() {
+        final String testClassName = this.getClass().getName();
+        assertTrue(testClassName.endsWith("Test"));
+
+        final String className = testClassName.substring(0, testClassName.length() - 4);
+
+        try {
+            return Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            fail(e);
+            return null;
+        }
+    }
+
+    @Test
+    default void ofTest() throws Throwable {
+        final Class<?> mapType = mapType();
+        if (mapType == null) {
+            return;
+        }
+
+        final var lookup = MethodHandles.publicLookup();
+        final var of0 = lookup.findStatic(mapType, "of", MethodType.methodType(mapType));
+        final var of1 = lookup.findStatic(mapType, "of", MethodType.methodType(mapType,
+                Object.class, Object.class
+        ));
+        final var of2 = lookup.findStatic(mapType, "of", MethodType.methodType(mapType,
+                Object.class, Object.class,
+                Object.class, Object.class
+        ));
+        final var of3 = lookup.findStatic(mapType, "of", MethodType.methodType(mapType,
+                Object.class, Object.class,
+                Object.class, Object.class,
+                Object.class, Object.class
+        ));
+        final var of4 = lookup.findStatic(mapType, "of", MethodType.methodType(mapType,
+                Object.class, Object.class,
+                Object.class, Object.class,
+                Object.class, Object.class,
+                Object.class, Object.class
+        ));
+        final var of5 = lookup.findStatic(mapType, "of", MethodType.methodType(mapType,
+                Object.class, Object.class,
+                Object.class, Object.class,
+                Object.class, Object.class,
+                Object.class, Object.class,
+                Object.class, Object.class
+        ));
+
+        Map<Integer, String> map;
+
+        map = (Map<Integer, String>) of0.invoke();
+        assertTrue(map.isEmpty());
+
+        map = (Map<Integer, String>) of1.invoke(0, "0");
+        assertEquals(Map.of(0, "0"), map);
+
+        map = (Map<Integer, String>) of2.invoke(0, "0", 1, "1");
+        assertEquals(Map.of(0, "0", 1, "1"), map);
+
+        map = (Map<Integer, String>) of3.invoke(0, "0", 1, "1", 2, "2");
+        assertEquals(Map.of(0, "0", 1, "1", 2, "2"), map);
+
+        map = (Map<Integer, String>) of4.invoke(0, "0", 1, "1", 2, "2", 3, "3");
+        assertEquals(Map.of(0, "0", 1, "1", 2, "2", 3, "3"), map);
+
+        map = (Map<Integer, String>) of5.invoke(0, "0", 1, "1", 2, "2", 3, "3", 4, "4");
+        assertEquals(Map.of(0, "0", 1, "1", 2, "2", 3, "3", 4, "4"), map);
+    }
+
+    @Test
+    default void ofEntriesTest() throws Throwable {
+        final Class<?> mapType = mapType();
+        if (mapType == null) {
+            return;
+        }
+
+        final var lookup = MethodHandles.publicLookup();
+        final var of0 = lookup.findStatic(mapType, "ofEntries", MethodType.methodType(mapType));
+        final var of1 = lookup.findStatic(mapType, "ofEntries", MethodType.methodType(mapType, Tuple2.class));
+        final var of2 = lookup.findStatic(mapType, "ofEntries", MethodType.methodType(mapType, Tuple2.class, Tuple2.class));
+        final var of3 = lookup.findStatic(mapType, "ofEntries", MethodType.methodType(mapType, Tuple2.class, Tuple2.class, Tuple2.class));
+        final var of4 = lookup.findStatic(mapType, "ofEntries", MethodType.methodType(mapType, Tuple2.class, Tuple2.class, Tuple2.class, Tuple2.class));
+        final var of5 = lookup.findStatic(mapType, "ofEntries", MethodType.methodType(mapType, Tuple2.class, Tuple2.class, Tuple2.class, Tuple2.class, Tuple2.class));
+        final var ofAll = lookup.findStatic(mapType, "ofEntries", MethodType.methodType(mapType, Tuple2[].class));
+
+        Map<Integer, String> map;
+
+        map = (Map<Integer, String>) of0.invoke();
+        assertTrue(map.isEmpty());
+
+        map = (Map<Integer, String>) of1.invoke(Tuple.of(0, "0"));
+        assertEquals(Map.of(0, "0"), map);
+
+        map = (Map<Integer, String>) of2.invoke(Tuple.of(0, "0"), Tuple.of(1, "1"));
+        assertEquals(Map.of(0, "0", 1, "1"), map);
+
+        map = (Map<Integer, String>) of3.invoke(Tuple.of(0, "0"), Tuple.of(1, "1"), Tuple.of(2, "2"));
+        assertEquals(Map.of(0, "0", 1, "1", 2, "2"), map);
+
+        map = (Map<Integer, String>) of4.invoke(Tuple.of(0, "0"), Tuple.of(1, "1"), Tuple.of(2, "2"), Tuple.of(3, "3"));
+        assertEquals(Map.of(0, "0", 1, "1", 2, "2", 3, "3"), map);
+
+        map = (Map<Integer, String>) of5.invoke(Tuple.of(0, "0"), Tuple.of(1, "1"), Tuple.of(2, "2"), Tuple.of(3, "3"), Tuple.of(4, "4"));
+        assertEquals(Map.of(0, "0", 1, "1", 2, "2", 3, "3", 4, "4"), map);
+
+        for (Integer[] data : data1()) {
+            Tuple2<Integer, String>[] array = (Tuple2<Integer, String>[])
+                    Stream.of(data).map(it -> Tuple.of(it, String.valueOf(it))).toArray(Tuple2<?, ?>[]::new);
+
+            //noinspection ConfusingArgumentToVarargsMethod
+            map = (Map<Integer, String>) ofAll.invoke(array);
+
+            assertEquals(data.length, map.size());
+            assertEquals(Map.ofEntries(array), map);
+        }
     }
 }
